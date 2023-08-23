297134cfc3d30cfe12b0d4fba50bf6353df36ebd
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2023-08-23 23:58:39 +0200

    added cluster debugger

--- kyonrlstepping/scripts/debugger.py
+++ kyonrlstepping/scripts/debugger.py
@@ -1,3 +1,13 @@
-from kyonrlstepping.utils.rt_debugging import RtDebugger
+from control_cluster_utils.utilities.cluster_debugger import RtClusterDebugger
 
-rt_debugger = RtDebugger(window_size=100)
+if __name__ == "__main__":  
+
+    update_dt = 0.05
+    window_length = 4.0
+    window_buffer_factor = 1
+    main_window = RtClusterDebugger(update_dt=update_dt,
+                            window_length=window_length, 
+                            window_buffer_factor=window_buffer_factor, 
+                            verbose=True)
+
+    main_window.run()
