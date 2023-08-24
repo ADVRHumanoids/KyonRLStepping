c0e982d7df4763f3d44bcabbc92fea69b5ae6b39
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2023-08-24 13:13:37 +0200

    updated to latest debugger features

--- kyonrlstepping/scripts/debugger.py
+++ kyonrlstepping/scripts/debugger.py
@@ -2,10 +2,14 @@ from control_cluster_utils.utilities.cluster_debugger import RtClusterDebugger
 
 if __name__ == "__main__":  
 
-    update_dt = 0.05
-    window_length = 4.0
-    window_buffer_factor = 1
-    main_window = RtClusterDebugger(update_dt=update_dt,
+    data_update_dt = 0.01
+    plot_update_dt = 0.1
+
+    window_length = 10.0
+    window_buffer_factor = 2
+
+    main_window = RtClusterDebugger(data_update_dt=data_update_dt,
+                            plot_update_dt=plot_update_dt,
                             window_length=window_length, 
                             window_buffer_factor=window_buffer_factor, 
                             verbose=True)
