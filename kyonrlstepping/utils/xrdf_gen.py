8e88cc1d1bbbc03afbf76a5fb6d55088bbdf8f34
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2023-11-24 16:39:47 +0100

    Signed-off-by: Andrea Patrizi <andreapatrizi1b6e6@gmail.com>

--- kyonrlstepping/utils/xrdf_gen.py
+++ kyonrlstepping/utils/xrdf_gen.py
@@ -1,5 +1,5 @@
 def get_xrdf_cmds_isaac(n_robots: int, 
-                robot_pkg_name: str,
+                robot_pkg_name: str = None,
                 basename = "kyon"):
 
         cmds = {}
@@ -21,10 +21,12 @@ def get_xrdf_cmds_isaac(n_robots: int,
         cmds_aux.append("floating_joint:=" + floating_joint)
         cmds_aux.append("payload:=" + payload)
 
-        import rospkg
-        rospackage = rospkg.RosPack()
-        package_root_path = rospackage.get_path(robot_pkg_name + "_urdf")
-        cmds_aux.append("kyon_root:=" + package_root_path)
+        if robot_pkg_name is not None:
+
+                import rospkg
+                rospackage = rospkg.RosPack()
+                package_root_path = rospackage.get_path(robot_pkg_name + "_urdf")
+                cmds_aux.append("kyon_root:=" + package_root_path)
 
         for i in range(n_robots):
                 # we use the same settings for all robots
@@ -32,7 +34,7 @@ def get_xrdf_cmds_isaac(n_robots: int,
 
         return cmds
 
-def get_xrdf_cmds_horizon(robot_pkg_name: str):
+def get_xrdf_cmds_horizon(robot_pkg_name: str = None):
 
         cmds = []
         
@@ -45,17 +47,19 @@ def get_xrdf_cmds_horizon(robot_pkg_name: str):
         sensors = "true" if xrdf_cmd_vals[3] else "false"
         floating_joint = "true" if xrdf_cmd_vals[4] else "false"
         payload = "true" if xrdf_cmd_vals[5] else "false"
-
+                
         cmds.append("wheels:=" + wheels)
         cmds.append("upper_body:=" + upper_body)
         cmds.append("dagana:=" + gripper)
         cmds.append("sensors:=" + sensors)
         cmds.append("floating_joint:=" + floating_joint)
         cmds.append("payload:=" + payload)
+        
+        if robot_pkg_name is not None:
 
-        import rospkg
-        rospackage = rospkg.RosPack()
-        package_root_path = rospackage.get_path(robot_pkg_name + "_urdf")
-        cmds.append("kyon_root:=" + package_root_path)
+                import rospkg
+                rospackage = rospkg.RosPack()
+                package_root_path = rospackage.get_path(robot_pkg_name + "_urdf")
+                cmds.append("kyon_root:=" + package_root_path)
 
         return cmds
