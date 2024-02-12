ea7afd16d5449e04e4bcf65e6406dad2328d5279
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-02-12 12:58:19 +0100

    added core affinity for gui

--- kyonrlstepping/scripts/RunDebugGUI.py
+++ kyonrlstepping/scripts/RunDebugGUI.py
@@ -1,7 +1,28 @@
 from control_cluster_bridge.utilities.debugger_gui.cluster_debugger import RtClusterDebugger
 from control_cluster_bridge.utilities.debugger_gui.gui_exts import JntImpMonitor
 
+import os
+import argparse
+
+# Function to set CPU affinity
+def set_affinity(cores):
+    try:
+        os.sched_setaffinity(0, cores)
+        print(f"Set CPU affinity to cores: {cores}")
+    except Exception as e:
+        print(f"Error setting CPU affinity: {e}")
+
 if __name__ == "__main__":  
+    
+    # Parse command line arguments for CPU affinity
+    parser = argparse.ArgumentParser(description="Set CPU affinity for the script.")
+    parser.add_argument('--cores', nargs='+', type=int, help='List of CPU cores to set affinity to')
+    
+    args = parser.parse_args()
+    
+    # Set CPU affinity if cores are provided
+    if args.cores:
+        set_affinity(args.cores)
 
     data_update_dt = 0.01
     plot_update_dt = 0.1
