100a410c7aa07a17deca48a8d20332f95ce24699
AndrePatri <andreapatrizi1b6e6@gmail.com>
2024-02-24 17:20:37 +0100

    now also supporting running rhc bridge for ros2

--- kyonrlstepping/scripts/RunRhc2RosBridge.py
+++ kyonrlstepping/scripts/RunRhc2RosBridge.py
@@ -1,16 +1,5 @@
-from lrhc_control.utils.rhc2viz import RhcToVizBridge
-
-from perf_sleep.pyperfsleep import PerfSleep
-
-from SharsorIPCpp.PySharsorIPC import LogType
-from SharsorIPCpp.PySharsorIPC import Journal
-
 import argparse
 import os
-
-import rospy
-
-import time 
 
 # Function to set CPU affinity
 def set_affinity(cores):
@@ -29,6 +18,8 @@ if __name__ == '__main__':
     parser.add_argument('robot_name', type=str, help='Name of the robot')
     parser.add_argument('--debug', action='store_true', help='Enable debug mode, default is False')
     parser.add_argument('--verbose', type=bool, default=True, help='Enable verbose mode, default is True')
+    parser.add_argument('--ros2', action='store_true', help='Enable ROS 2 mode')
+
     args = parser.parse_args()
 
     # Set CPU affinity if cores are provided
@@ -41,54 +32,22 @@ if __name__ == '__main__':
     debug = args.debug
     verbose = args.verbose
 
-    bridge = RhcToVizBridge(namespace=robot_name, 
+    bridge = None
+    if not args.ros2:
+
+        from lrhc_control.utils.rhc_viz.rhc2viz import RhcToVizBridge
+
+        bridge = RhcToVizBridge(namespace=robot_name, 
                         verbose=verbose,
                         rhcviz_basename="RHCViz", 
                         robot_selector=[0, None])
-    
-    bridge.run()
+    else:
 
-    perf_timer = PerfSleep()
-    
-    info = f": starting bridge with update dt {update_dt} s"
-    Journal.log("RHC2RosBridge",
-        "",
-        info,
-        LogType.INFO,
-        throw_when_excep = True)
+        from lrhc_control.utils.rhc_viz.rhc2viz2 import RhcToViz2Bridge
 
-    start_time = 0.0
-    elapsed_time = 0.0
-    actual_loop_dt = 0.0
+        bridge = RhcToViz2Bridge(namespace=robot_name, 
+                        verbose=verbose,
+                        rhcviz_basename="RHCViz", 
+                        robot_selector=[0, None])
 
-    time_to_sleep_ns = 0
-
-    while not rospy.is_shutdown():
-
-        start_time = time.perf_counter() 
-
-        success = bridge.update() # index is the controller 
-        # position in the cluster
-
-        elapsed_time = time.perf_counter() - start_time
-
-        time_to_sleep_ns = int((update_dt - elapsed_time) * 1e+9) # [ns]
-
-        if time_to_sleep_ns < 0:
-
-            warning = f": Could not match desired update dt of {update_dt} s. " + \
-                f"Elapsed time to update {elapsed_time}."
-            Journal.log("RHC2RosBridge",
-                "",
-                warning,
-                LogType.WARN,
-                throw_when_excep = True)
-
-        perf_timer.thread_sleep(time_to_sleep_ns) # nanoseconds (actually resolution is much
-                    # poorer)
-
-        actual_loop_dt = time.perf_counter() - start_time
-
-        if debug:
-
-            print(f"Actual loop dt {actual_loop_dt} s.")
+    bridge.run(update_dt=update_dt)
