cf8fc2271e41a69c9c7114d6adca19c80c0f1698
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-10-02 19:30:43 +0200

    added cost for z also during stances

--- kyonrlstepping/controllers/horizon_based/kyon_rhc.py
+++ kyonrlstepping/controllers/horizon_based/kyon_rhc.py
@@ -217,6 +217,9 @@ class KyonRhc(HybridQuadRhc):
             stance_phase_short = self._c_timelines[c].createPhase(short_stance_duration, f'stance_{c}_short')
             if self._ti.getTask(f'{c}') is not None:
                 stance_phase_short.addItem(self._ti.getTask(f'{c}'))
+                ref_trj = np.zeros(shape=[7, short_stance_duration])
+                stance_phase_short.addItemReference(self._ti.getTask(f'z_{c}'),
+                    ref_trj, nodes=list(range(0, short_stance_duration)))
             else:
                 Journal.log(self.__class__.__name__,
                     "_init_contact_timelines",
@@ -243,6 +246,10 @@ class KyonRhc(HybridQuadRhc):
             if not post_landing_stance<1:
                 if self._ti.getTask(f'{c}') is not None:
                     flight_phase.addItem(self._ti.getTask(f'{c}'), nodes=list(range(flight_duration, flight_duration+post_landing_stance)))
+                    ref_trj = np.zeros(shape=[7, post_landing_stance])
+                    flight_phase.addItemReference(self._ti.getTask(f'z_{c}'),
+                        ref_trj,
+                        nodes=list(range(flight_duration, flight_duration+post_landing_stance)))
                     i=0
                     for force in self._ti.model.cmap[c]:
                         force_reg=self._prb.getCosts(f'{c}_force_reg_f{i}')
