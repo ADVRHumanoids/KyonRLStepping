97c83cfce2f623c5c2d715c562134073b678d45a
AndrePatri <andreapatrizi97@gmail.com>
2026-03-20 11:09:20 +0100

    more aggressive tracking

--- kyonrlstepping/controllers/horizon_based/b2w_rhc.py
+++ kyonrlstepping/controllers/horizon_based/b2w_rhc.py
@@ -97,8 +97,7 @@ class B2WRhc(HybridQuadRhc):
             foot_linkname="FL_foot",
             flight_duration=flight_duration,
             post_flight_stance=post_flight_duration,
-            step_height=0.12,
+            step_height=0.15,
             keep_yaw_vert=False,
-            yaw_vertical_weight=1e-6,
             phase_force_reg=2e-2,
             vel_bounds_weight=1.0)
